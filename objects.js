b0769d8b807f9e53f946d45369fd65405615f284
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-19 13:50:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,10 +4,12 @@ playlist['My Bloody Valentine']='Sometimes';
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName]=singTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
+  return playlist;
 }
 
 
